e15327cf3779b7156474c7937afe984e0ad182d4
tluwaniMS <tluwani2020@gmail.com>
2023-11-06 07:52:52 +0200

    adding-basic-comments-in-scripts

--- bash-practice-scripts/elif-script-example.sh
+++ bash-practice-scripts/elif-script-example.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+# This is a basic bash-script to give one a quick guide of how to use else if statement.
--- bash-practice-scripts/user-input-with-options-script-example.sh
+++ bash-practice-scripts/user-input-with-options-script-example.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+# This is a basic bash-script to give one a quick guide of how to read user-input from command line with options.
