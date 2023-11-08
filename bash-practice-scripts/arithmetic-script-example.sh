717cadd20cbd91eb54b4c886dd3cf360f7604ca9
tluwaniMS <tluwani2020@gmail.com>
2023-11-08 07:44:43 +0200

    adding-arithmetic-script-examples

--- bash-practice-scripts/arithmetic-script-example.sh
+++ bash-practice-scripts/arithmetic-script-example.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 # This is a basic bash-script to give one a quick guide of how to use arithmetic.
+
+let summed_v1=2+3
+
+echo $summed_v1
+
+summed_v2=$(( 5 + 8))
+
+echo $summed_v2
+
+let "summed_v3=6+2"
+
+echo $summed_v3
+
+echo $(( 2 + 1 ))
