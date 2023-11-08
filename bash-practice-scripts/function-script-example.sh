bc4b3ea9ea1d623c1de914d19885ce2ea80dec61
tluwaniMS <tluwani2020@gmail.com>
2023-11-08 08:58:46 +0200

    adding-function-script-examples

--- bash-practice-scripts/function-script-example.sh
+++ bash-practice-scripts/function-script-example.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
 # This is a basic bash-script to give one a quick guide of how to create a function.
+
+greetTheWorld(){
+    echo "The world says hello $1"
+}
+
+greetTheWorldFully(){
+    echo "The world is giving a full hello $1 $2"
+}
+
+function greetTheWorldNamed(){
+    echo "The named world says hello $1"
+}
+
+function greetTheWorldFullyNamed(){
+    echo "The named world is giving a full hello $1 $2"
+}
+
+greetTheWorld Thembekile
+greetTheWorldFully Thembekile Luvenga
+
+greetTheWorldNamed John
+greetTheWorldFullyNamed John Doe
