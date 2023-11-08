56cf898a36c42d7666ab39603c5f653d646cca35
tluwaniMS <tluwani2020@gmail.com>
2023-11-08 08:44:18 +0200

    adding-switch-case-statement-example

--- bash-practice-scripts/switch-case-script-example.sh
+++ bash-practice-scripts/switch-case-script-example.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
 # This is a basic bash-script to give one a quick guide of how to use switch case.
+
+progress="Complete"
+
+case $progress in
+    "Started")
+        echo "The process has started"
+        ;;
+    "In Progress")
+        echo "The process is in progress"
+        ;;
+    "Pending")
+        echo "The process is pending"
+        ;;
+    "Complete")
+        echo "The process is complete"
+        ;;
+    *)
+        echo "The progress state is undefined"
+        ;;
+esac
