bc905a8e60c429715293ccf25263d6d6313f76a7
tluwaniMS <tluwani2020@gmail.com>
2023-11-08 06:04:06 +0200

    adding-user-input-bash-script-example

--- bash-practice-scripts/user-input-with-options-script-example.sh
+++ bash-practice-scripts/user-input-with-options-script-example.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 # This is a basic bash-script to give one a quick guide of how to read user-input from command line with options.
+
+read -p 'Please provide your name: ' name
+read -p 'Please provide your surname: ' surname
+read -sp 'Provide a private/secret input: ' secret_input
+
+echo "The name provided: $name"
+
+echo "The surname provided: $surname"
+
+echo "The provided secret: $secret_input"
