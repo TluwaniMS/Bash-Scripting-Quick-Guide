d9e2597ada51aab38f3861f19896a19df149d52d
tluwaniMS <tluwani2020@gmail.com>
2023-11-08 07:30:55 +0200

    adding-elif-example

--- bash-practice-scripts/elif-script-example.sh
+++ bash-practice-scripts/elif-script-example.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 # This is a basic bash-script to give one a quick guide of how to use else if statement.
+
+num=4
+
+if (($num <= 5)); then
+    echo "We are running within the 5th."
+elif (($num >= 7 && $num < 8)); then
+    echo "We are running within the 7th."
+else
+    echo "The seventh has been surpassed."
+fi
